19df648f933cb3be31080ed4c5b78abcd65d31cb
QuangNhatAS <doanquangnhat97@gmail.com>
2021-04-28 00:43:23 +0700

    refactor code in loading_screen

---
+++ lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double lat;
+  double long;
+
+  Location({this.lat, this.long});
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      lat = position.latitude;
+      long = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
